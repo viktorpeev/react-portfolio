b1a1fe3294467f663e46afeeb5aeaf3847d70e63
Viktor <lulzsec123@yahoo.com>
2023-01-07 10:55:04 +0200

    Navigation styling and links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,50 @@
-import './index.scss'
-import { Link } from 'react-router-dom';
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 export const Sidebar = () => {
     return (
         <div className='nav-bar'>
             <Link className='logo' to='/'>
-                <img src={LogoS} alt='logo'/>
+                <img src={LogoS} alt='logo' />
                 <img className='sub-logo' src={LogoSubtitle} />
             </Link>
+            <nav>
+                <NavLink exact='true' activeClassName='active' to='/'>
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact='true' activeClassName='active' to='/about'>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                </NavLink>
+                <NavLink activeClassName='active' to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target='_blank'
+                        rel='noreferrer'
+                        href='https://www.linkedin.com/in/viktor-peev-5113061b5/'
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://www.facebook.com/viktor.peev.75'
+
+                    
+                    >
+                        <FontAwesomeIcon icon={faFacebook} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 }
